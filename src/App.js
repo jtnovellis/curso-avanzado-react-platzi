4b6a56877d445c7b91071846014b4ffbdc7f144f
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-01-10 21:42:48 -0500

    add list of categories and global styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
-import { Category } from './components/Category'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyles'
 
 export const App = () => {
-  return <Category />
+  return (
+    <div>
+      <GlobalStyle />
+      <ListOfCategories />
+    </div>
+  )
 }
