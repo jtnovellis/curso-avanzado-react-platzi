ebf6dc6c580cde2e6ec7da5aac4c9960c9ce6a2d
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-01-11 01:54:12 -0500

    custom hook to lazy load with intersection observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import { ImgWrapper, Image, Button } from './styles'
+import React, { useRef, useEffect, useState } from 'react'
+import { ImgWrapper, Image, Button, Article } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const [show, setShow] = useState(false)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(ref.current)
+  }, [ref])
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Image src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+    <Article ref={ref}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Image src={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
   )
 }
