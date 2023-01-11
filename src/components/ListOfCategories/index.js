d74749f784e476f0262851903713c85243bb7696
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-01-10 22:15:31 -0500

    add photocard and list

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
+import db from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
     <List>
-      {[1, 2, 3].map((category) => (
-        <Item key={category}>
-          <Category />
+      {db.categories.map((category) => (
+        <Item key={category.id}>
+          <Category {...category} />
         </Item>
       ))}
     </List>
